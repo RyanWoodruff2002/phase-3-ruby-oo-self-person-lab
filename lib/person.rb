e54ce50b007506778005068788cc8b081a6e4e97
Ryan Woodruff <ryanwoodruff2002@gmail.com>
2022-08-11 17:22:14 -0400

    commit tree b29bcdcd6e131950d6d358891713fca8c1302d23

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,34 @@
 # your code goes here
+
+class Person
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+    attr_reader :name
+    attr_accessor :bank_account
+    attr_accessor :happiness
+    attr_accessor :hygiene
+
+    def happy?
+        if happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def self.get_paid(amount)
+        bank_account + amount
+    end
+end
